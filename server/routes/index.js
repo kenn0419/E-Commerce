f9a71126e49abd18a04568971b779aa5bad3df94
kenn3108 <tranbao12369@gmail.com>
2023-11-13 13:51:20 +0700

    #3 hash Password and errorHandler

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,12 @@
 const userRouter = require('./user');
+const { notFound, errorHandler } = require('../middlewares/errorHandler')
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter);
+
+
+    app.use(notFound);
+    app.use(errorHandler);
 }
 
 module.exports = initRoutes
