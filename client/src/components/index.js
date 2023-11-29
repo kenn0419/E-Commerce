a354babc2cbd817a82c578a21a1e9c6713139262
kenn3108 <tranbao12369@gmail.com>
2023-11-29 13:51:34 +0700

    #21 base-react

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
+export { default as Baner } from './Baner';
+export { default as Navigation } from './Navigation';
